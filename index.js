1f44a03ebfecef06948e984a84a33f3426f2d7f7
jamesphenderson <elmertheriot@gmail.com>
2020-03-22 16:44:06 +0300

    BN: bare minimum tab navigation

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
-import React from "react"
+import React from 'react';
 import App from './App';
 import {name as appName} from './app.json';
-import {Provider} from "react-redux"
-import {store} from "./src/store"
+import {Provider} from 'react-redux';
+import {store} from './src/store';
 
 const ReduxWrapper = () => {
-    return (
-        <Provider store={store}>
-            <App/>
-        </Provider>
-    )
-}
+  return (
+    <Provider store={store}>
+        <App />
+    </Provider>
+  );
+};
 
 AppRegistry.registerComponent(appName, () => ReduxWrapper);

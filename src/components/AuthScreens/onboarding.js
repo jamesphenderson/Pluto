1e83a3e689a1515cf6fdd5b5f67e2b13f42ae596
jamesphenderson <elmertheriot@gmail.com>
2020-03-22 22:02:37 +0300

    BN: added permissions slider + btns to onboarding; improved btn text

--- src/components/AuthScreens/onboarding.js
+++ src/components/AuthScreens/onboarding.js
@@ -1,38 +1,12 @@
 import React from 'react';
-import {View, Text, Button, Image} from 'react-native';
+import {View} from 'react-native';
 import AppIntroSlider from 'react-native-app-intro-slider';
-
-const slides = [
-  {
-    key: 'intro',
-    title: 'Intro',
-    text: 'Description.\nSay something cool',
-    image: require('./../../../assets/img/music.png'),
-    backgroundColor: '#59b2ab',
-    imageStyle: {width: 400, height: 400, resizeMode: 'contain'}
-  },
-  {
-    key: 'purpose',
-    title: 'Purpose',
-    text: 'Elaborate\nShare what your app does',
-    image: require('./../../../assets/img/music.png'),
-    backgroundColor: '#febe29',
-    imageStyle: {width: 400, height: 400, resizeMode: 'contain'}
-  },
-  {
-    key: 'finish',
-    title: 'Blastoff',
-    text: "Prologue\nEnd with a bang",
-    image: require('./../../../assets/img/music.png'),
-    backgroundColor: '#22bcb5',
-    imageStyle: {width: 400, height: 400, resizeMode: 'contain'}
-  }
-];
+import {slides} from "./../../utils/onboarding/slides"
 
 const IntroSlider = ({navigation}) => {
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <AppIntroSlider slides={slides} onDone={() => navigation.navigate("Login")}/>
+      <AppIntroSlider slides={slides} bottomButton={true} doneLabel={"Complete"} onDone={() => navigation.navigate("Login")}/>
     </View>
   );
 };
---
+++ src/utils/onboarding/slides.js
@@ -0,0 +1,34 @@
+export const slides = [
+    {
+      key: 'intro',
+      title: 'Intro',
+      text: 'Precursor.\nSome cool starter text',
+      image: require('./../../../assets/img/music.png'),
+      backgroundColor: '#59b2ab',
+      imageStyle: {width: 400, height: 400, resizeMode: 'contain'}
+    },
+    {
+      key: 'purpose',
+      title: 'Purpose',
+      text: 'Elaborate\nShare what your app does',
+      image: require('./../../../assets/img/music.png'),
+      backgroundColor: '#febe29',
+      imageStyle: {width: 400, height: 400, resizeMode: 'contain'}
+    },
+    {
+        key: 'permissions',
+        title: 'Permissions',
+        text: 'Grants\nRequest for access to sys',
+        image: require('./../../../assets/img/music.png'),
+        backgroundColor: '#febe29',
+        imageStyle: {width: 400, height: 400, resizeMode: 'contain'}
+      },
+    {
+      key: 'finish',
+      title: 'Finish',
+      text: "Prologue\nEnd with a bang",
+      image: require('./../../../assets/img/music.png'),
+      backgroundColor: '#22bcb5',
+      imageStyle: {width: 400, height: 400, resizeMode: 'contain'}
+    }
+  ];

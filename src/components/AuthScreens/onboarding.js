226e4cb1fae697668e52dca94416c6aa25c6e991
jamesphenderson <elmertheriot@gmail.com>
2020-03-23 14:56:22 +0300

    BN: working android permissions request

--- src/components/AuthScreens/onboarding.js
+++ src/components/AuthScreens/onboarding.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text} from 'react-native';
 import AppIntroSlider from 'react-native-app-intro-slider';
 import {slides} from './../../utils/onboarding/slides';
 
+// utils
+import {requestCameraPermissions} from "./../../utils/permissions"
+
 const IntroSlider = ({navigation}) => {
+  const [currentSlideIndex, updateCurrentSlideIndex] = useState(0)
+  // if(currentSlideIndex === 2) requestCameraPermissions()
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <AppIntroSlider
@@ -11,8 +16,10 @@ const IntroSlider = ({navigation}) => {
         bottomButton={true}
         buttonStyle={{backgroundColor: '#444'}}
         doneLabel={'Complete'}
+        onSlideChange={index => updateCurrentSlideIndex(index)}
         onDone={() => navigation.navigate('Login')}
       />
+      <Text>Slide {currentSlideIndex}</Text>
     </View>
   );
 };

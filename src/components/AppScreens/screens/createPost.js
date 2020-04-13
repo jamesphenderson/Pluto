c635fa029f64b8be682596cdc1c2c5682450b57c
jamesphenderson <elmertheriot@gmail.com>
2020-04-14 00:11:42 +0300

    BN: implemented play/pause of mic animation in createPost component (working bare minimum)

--- src/components/AppScreens/screens/createPost.js
+++ src/components/AppScreens/screens/createPost.js
@@ -1,20 +1,47 @@
-import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import React, {Component} from 'react';
+import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
 import LottieView from 'lottie-react-native';
 
-const CreatePost = ({navigation}) => {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#fff',
-      }}>
-        <LottieView source={require('./../../../../assets/animations/mic.json')} autoPlay loop />
-      <Text>Create Post</Text>
-    </View>
-  );
-};
+class CreatePost extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isPlaying: false,
+    };
+  }
+  startAnimation = () => {
+    this.animation.play();
+    this.setState({isPlaying: true});
+  };
+  stopAnimation = () => {
+    this.animation.reset();
+    this.setState({isPlaying: false});
+  };
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: '#fff',
+        }}>
+        <TouchableOpacity
+          activeOpacity={1}
+          onPress={
+            this.state.isPlaying ? this.stopAnimation : this.startAnimation
+          }>
+          <LottieView
+            source={require('./../../../../assets/animations/mic.json')}
+            ref={animation => {
+              this.animation = animation;
+            }}
+            style={{height: 250, width: 250}}
+          />
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
 
 export default CreatePost;
